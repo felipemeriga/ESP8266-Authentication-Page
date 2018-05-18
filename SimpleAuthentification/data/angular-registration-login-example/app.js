be8cdfed67030775f8000336626aa74d13bdbd72
Felipemeriga <felipe.meriga@gmail.com>
2018-05-17 21:57:51 -0300

    refractoring

---
+++ SimpleAuthentification/data/angular-registration-login-example/app.js
@@ -0,0 +1,82 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+			
+			
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookies', '$http','UserService'];
+    function run($rootScope, $location, $cookies, $http, UserService) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+		$rootScope.wifiSettingsGlobals = $cookies.getObject('wifiSettings') || {};
+		$rootScope.connectedWifi = false;
+		
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+        }
+		
+		UserService.getConnectionStatus().then(function (response) {
+			if(response.status == 200){
+				if(response.data == true){
+					$rootScope.connectedWifi = true;
+				}
+				else{
+					$rootScope.connectedWifi = false;
+				}
+			}
+		});
+		
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+			if(!$rootScope.connectedWifi){
+				if($rootScope.wifiSettingsGlobals.ssid){
+					UserService.passWifiCredentialsConnect($rootScope.wifiSettingsGlobals).then(function (response) {
+						if(response.status == 200 || response.status == 204){
+							console.log('Connected Successfully with' + $rootScope.wifiSettingsGlobals.ssid );
+							$location.path('/home');
+							$rootScope.connectedWifi = true;
+						}
+						else{
+							$cookies.remove('wifiSettings');
+							$location.path('/login');
+						}
+					});
+				}
+				else{
+					$location.path('/login');
+				}
+				
+			}
+        });
+    }
+
+})();
---
+++ SimpleAuthentification/data/angular-registration-login-example/login/login.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService','$rootScope'];
+    function LoginController($location, AuthenticationService, FlashService, $rootScope) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.ssid, vm.password, function (response) {
+                if (response.success) {
+					$rootScope.connectedWifi = true;
+                    AuthenticationService.SetCredentials(vm.ssid, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
